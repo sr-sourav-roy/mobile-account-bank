a472774bb75b79ca88ad29c294ab6f3285c80bf8
sr-sourav-roy <sourav.cp.bd@gmail.com>
2026-03-03 08:11:01 +0600

    add by add by create new fucntion utilites

--- java-script.js/addmoney.js
+++ java-script.js/addmoney.js
@@ -5,9 +5,14 @@ document
     const accountNumber = document.getElementById("account-numbers").value;
     const amount = getInputValueByID("amount");
     const pin = getInputValueByID("pin");
+    const mainBalance = getInnerTextById("main-balance");
 
     if (accountNumber.length === 11) {
       if (pin === 1234) {
+        const sum = mainBalance + amount;
+        // document.getElementById("main-balance").innerText = sum;
+        // the function call by id and value add:
+        setInnerTextIDandValue("main-balance", sum);
       } else {
         alert("wrong pin");
       }
--- utilities/utilites.js
+++ utilities/utilites.js
@@ -1,5 +1,18 @@
+// create value functions:
 function getInputValueByID(id) {
   const value = document.getElementById(id).value;
   const convertedValue = parseFloat(value);
   return convertedValue;
+}
+
+// function create by InnerText:
+function getInnerTextById(id) {
+  const innerText = document.getElementById(id).innerText;
+  const convertInnerText = parseFloat(innerText);
+  return convertInnerText;
+}
+
+// set element by ID and value just create a functions: value set korar karon hocche amra je konw man gula add korar jonnw!
+function setInnerTextIDandValue(id, value) {
+  document.getElementById(id).innerText = value;
 }
